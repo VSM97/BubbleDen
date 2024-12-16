4dc3e9e77b8d4c1455a43571b920a6ace697d1ba
VSM97 <39551654+VSM97@users.noreply.github.com>
2024-12-16 10:00:41 +0530

    Delete dist/dist directory

--- dist/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export type { BotProps } from './components/Bot';
-export type { BubbleProps } from './features/bubble/components/Bubble';
-export * from './types';
-export { init, initFull, destroy } from './window';
-export { registerWebComponents } from './register';
-//# sourceMappingURL=index.d.ts.map
